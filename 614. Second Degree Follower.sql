9c5cc15261f4e1ecc4b6fbd0375cdc007a711820
coco93 <jwu99@gatech.edu>
2017-09-02 16:32:35 -0400

    Update 614. Second Degree Follower.sql

--- 614. Second Degree Follower.sql
+++ 614. Second Degree Follower.sql
@@ -7,3 +7,12 @@ where followee in (
 select follower from follow)
 group by followee
 order by follower
+
+# 11/12 test case passed..
+
+select f1.followee as follower, count(*) as num
+from (select distinct * from follow) f1
+left join (select distinct * from follow) f2
+on f1.followee = f2.follower
+where f2.follower is not null
+group by f1.followee
