25efd4ae3d088364fdd26e9dbe8c2d3d6828671b
coco93 <jwu99@gatech.edu>
2017-09-01 21:25:03 -0400

    Update 178. Rank Scores.sql

--- 178. Rank Scores.sql
+++ 178. Rank Scores.sql
@@ -4,3 +4,9 @@ select Score,
 where Score >= s.Score) as Rank
 from Scores s
 order by Score DESC
+
+# solution 2  
+select s.Score, count(distinct(t.Score)) as Rank
+from Scores s join Scores t on s.Score <= t.Score
+group by s.Id
+order by s.Score DESC
