0aa6e74156f850c85a07f497ee2e9825bc8c1e9b
coco93 <jwu99@gatech.edu>
2017-09-04 13:04:13 -0400

    Update 177. Nth Highest Salary.sql

--- 177. Nth Highest Salary.sql
+++ 177. Nth Highest Salary.sql
@@ -9,3 +9,14 @@ set N = N-1;
       select ifnull ((select Salary from Employee order by Salary DESC limit 1 offset N), null)
      );
 END
+
+
+# solution 1
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+set N = N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select ifnull((select Salary from Employee group by Salary order by Salary DESC limit 1 offset N), null)
+     );
+END
