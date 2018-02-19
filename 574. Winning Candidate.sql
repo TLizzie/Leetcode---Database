d82fdf83a58881eaf2c73a57aa31d5c4933c60ce
coco93 <jwu99@gatech.edu>
2018-02-18 23:10:12 -0500

    Update 574. Winning Candidate.sql

--- 574. Winning Candidate.sql
+++ 574. Winning Candidate.sql
@@ -9,3 +9,13 @@ group by CandidateId
 order by count(*) DESC
 limit 1) t1
 where t1.CandidateId = c.id
+
+# 2/18/2018
+select Name from Candidate c
+inner join 
+(select v.CandidateId
+from Vote v
+group by CandidateId
+order by count(*) DESC
+limit 1) a
+on c.id = a.CandidateId 
