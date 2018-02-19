b245188162893d02afb9718222c283d52ee9b3ac
coco93 <jwu99@gatech.edu>
2018-02-19 13:45:28 -0500

    Update 580. Count Student Number in Departments.sql

--- 580. Count Student Number in Departments.sql
+++ 580. Count Student Number in Departments.sql
@@ -6,3 +6,8 @@ right join department d
 on s.dept_id = d.dept_id
 group by dept_name
 order by student_number DESC, dept_name 
+# 2/19/2018
+select d.dept_name as dept_name, count(s.student_name) as student_number
+from student s right join department d on s.dept_id = d.dept_id
+group by d.dept_name
+order by student_number desc, dept_name
