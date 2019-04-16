457a10a9910485dfeec855aee0662312eccb346a
TLizzie <42203546+TLizzie@users.noreply.github.com>
2019-04-16 15:55:06 -0700

    Update 175. Combine Two Tables.sql

--- 175. Combine Two Tables.sql
+++ 175. Combine Two Tables.sql
@@ -2,3 +2,11 @@
 
 select p.FirstName, p.LastName, a.City, a.State
 from person p left join address a on p.PersonId = a.PersonId
+
+
+#solution 2
+
+select Firstname, Lastname, city, state
+from person
+left join address
+on person.personID = address.personID
