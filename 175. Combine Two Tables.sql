654c6713e0b63d2e0f65c67a8cc4037266eaa562
TLizzie <42203546+TLizzie@users.noreply.github.com>
2019-04-16 15:55:56 -0700

    Update 175. Combine Two Tables.sql

--- 175. Combine Two Tables.sql
+++ 175. Combine Two Tables.sql
@@ -1,7 +1,9 @@
 # solution 1
 
 select p.FirstName, p.LastName, a.City, a.State
-from person p left join address a on p.PersonId = a.PersonId
+from person p 
+left join address a 
+on p.PersonId = a.PersonId
 
 
 #solution 2
